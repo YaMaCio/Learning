a195898bc7d18222da864fc75480a3997a6b3b45
YaMaCio <88288554+YaMaCio@users.noreply.github.com>
2024-02-29 11:10:01 +0200

    Create WebElements.py

---
+++ AT/WebElements.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+link = "https://testpages.eviltester.com/styled/index.html"
+browser = driver = webdriver.Chrome()
+browser.implicitly_wait(10)
+
+browser.get(link)
+driver.find_element(By.XPATH, "//a[contains(text(),'HTML5 Element Form Test Page')]").click()
+driver.find_element(By.ID, "colour-picker").send_keys("#000000")
+driver.find_element(By.ID, "date-picker").send_keys("2002-02-02")
+driver.find_element(By.ID, "date-time-picker").send_keys("2002-02-02T02:02")
+driver.find_element(By.ID, "email-field").send_keys("bob@mailinator.com")
+driver.find_element(By.ID, "month-field").send_keys("2002-02")
+driver.find_element(By.ID, "number-field").send_keys("42")
+driver.find_element(By.NAME, "submitbutton").click()
+assert driver.find_element(By.ID, "_valuecolour") == "#000000"
+assert driver.find_element(By.ID, "_valuedate") == "2002-02-02"
+assert driver.find_element(By.ID, "_valuedatetime") == "2002-02-02T02:02"
+assert driver.find_element(By.ID, "_valueemail") == "bob@mailinator.com"
+assert driver.find_element(By.ID, "_valuemonth") == "2002-02"
+assert driver.find_element(By.ID, "_valuenumber") == "42"
+
+browser.get(link)
+driver.find_element(By.ID, "basicauth").click()
+usernameEl = driver.find_element(By.XPATH, "//p[contains(text(),'username')]")
+username = usernameEl[10:]
+passwordEl = driver.find_element(By.XPATH, "//p[contains(text(),'password')]")
+password = usernameEl[10:]
+driver.find_element(By.XPATH, "//a[contains(text(),'Basic Auth Protected Page')]").click()
+assert driver.find_element(By.ID, "status") == "Authenticated"
+
+browser.get(link)
+driver.find_element(By.ID, "fileuploadtest").click()
+driver.find_element(By.ID, "itsafile").click()
+file = open(os.getcwd() + 'test' + '.txt', 'rb')
+driver.find_element(By.ID, "fileinput").send_keys(file)
+driver.find_element(By.NAME, "upload").click()
+assert driver.find_element(By.XPATH, "//p[contains(text(),'You uploaded a file. This is the result')]").is_displayed()
+
+browser.get(link)
+driver.find_element(By.ID, "alerttestjs").click()
+driver.find_element(By.ID, "alertexamples").click()
+Alert(driver).accept()
+assert driver.find_element(By.XPATH, "//p[contains(text(),'You triggered and handled the alert dialog')]").is_displayed()
+
+browser.quit()
